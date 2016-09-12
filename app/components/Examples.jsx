ebb34b0a343ae614b5a73e6b46c5bcfef5d01110
Rafa Marfil <marffox@gmail.com>
2016-09-12 21:47:06 +0200

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -11,7 +11,10 @@ var React = require('react');
 
 var Examples = (props) => {
 	return (
-		<h3>Examples component</h3>
+		<div>
+			<h3>Examples component</h3>
+			<p>Welcome to the example page</p>
+		</div>
 	);
 };
 
